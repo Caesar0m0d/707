8731ffa4efcafb228b28128c9c88d90dae18b1a3
Caesar0m0d <Caesar0m0d@gmail.com>
2017-04-07 23:06:44 +0900

    707B.c

---
+++ 707B.c
@@ -0,0 +1,25 @@
+include<stdio.h>
+include<stdlib.h>
+typedef struct N{
+
+int p,e,n,st_v;			//(previous,exist,next,stack value)
+int * stk;			//stk for dfs 
+
+}h;
+
+	int main(){
+int n,m,k,l,u,v;
+long long len;
+
+scanf("%d %d %d",&n,&m,&k);
+int * fl = (int *)malloc(sizeof(int)*k+1);
+		for(l=0;l<m;l++){
+scanf("%d %d %d",&u,&v,&len);
+				}
+		if(k > 0)for(l=0;l<k;l++){
+		if(l == k-1)scanf("%d",fl[l]);
+scanf("%d ",fl[l]);
+					 }
+
+return 0;
+		  }
